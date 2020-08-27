8354cf5a680e294359c142326c3630d321e04b55
unknown <zachariah.howarth-redmond@students.mq.edu.au>
2020-08-27 15:09:02 +1000

    Updated Workshop5

--- server/server.js
+++ server/server.js
@@ -48,8 +48,9 @@ app.put('/api/units/:id', (req, res) => {
   res.json(newUnit)
 })
 
-
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+app.use(express.static('build'))
--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import UnitForm from "./UnitForm.js"
 import Unit from "./Unit.js"
 import axios from 'axios'
 
+const baseUrl = 'http://localhost:3001/api/units'
+
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
+}
+
 
 const App = () => {
   
